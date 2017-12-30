49287b58a5cfc74c12db13e3dd82883044575c3f
Roman Malinovskiy <malinovskiyr@inbox.ru>
2017-12-30 16:28:54 +0300

    fix

--- Preview/dump_db_pickle_recs.py
+++ Preview/dump_db_pickle_recs.py
@@ -1,7 +1,7 @@
 import pickle, glob
 for filename in glob.glob('*.pkl'):         # for 'bob','sue','tom'
     recfile = open(filename, 'rb')
-    record  = pickle.load(recfile)
+    record = pickle.load(recfile)
     print(filename, '=>\n  ', record)
 
 suefile = open('sue.pkl', 'rb')
