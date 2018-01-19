cd7e492464285e86bfdef8d45bb6532b78ccf14a
Малиновский Роман Игоревич <C:\Users\malinovskiyri\AppData\Roaming\The Bat!>
2018-01-19 17:27:23 +0300

    fix

--- System/more.py
+++ System/more.py
@@ -2,13 +2,17 @@
 split and interactively page a string or file of text
 """
 
+
 def more(text, numlines=15):
     lines = text.splitlines()                # like split('\n') but no '' at end
     while lines:
         chunk = lines[:numlines]
         lines = lines[numlines:]
-        for line in chunk: print(line)
-        if lines and input('More?') not in ['y', 'Y']: break
+        for line in chunk:
+            print(line)
+        if lines and input('More?') not in ['y', 'Y']:
+            break
+
 
 if __name__ == '__main__':
     import sys                               # when run, not imported
